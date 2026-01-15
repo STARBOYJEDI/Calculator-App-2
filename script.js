bd186376de1f918e4c23917fdb00523fa8e025ab
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-01-15 18:49:32 +0200

    Clear previous operand display when no operation is selected

--- script.js
+++ script.js
@@ -79,6 +79,8 @@ class Calculator {
         if (this.operation != null) {
             this.previousOperandTextElement.innerText = 
                 `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
     }
 }
